e569c19a8398bdb35d0181b0d118ce4a72afa7cf
Lê Quang Thọ <54834124+LeQuangTho@users.noreply.github.com>
2021-04-14 10:09:48 +0700

    10.09 14/04/2021

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void StartScence()
+    {
+        Application.LoadLevel("PlayGame");
+    }
+
+   
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
